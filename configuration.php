0450737a26a5914893fe08731d2f362258316903
Mikleoo <60879715+Mikleoo@users.noreply.github.com>
2021-10-22 17:18:07 +0200

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -2,20 +2,20 @@
 // This class contains all the information to modify depending on the web host
 class Config {
 	public $sitename = 'Victor Louvet';
-  public $MetaDesc = '';
+	public $MetaDesc = '';
 	public $MetaKeys = '';
 	public $dbtype = 'mysql';
 	public $host = 'localhost';
 	public $user = 'root';
 	public $password = '';
-	public $dbname = 'victorlouvet_db';
-  public $dbport = "3306";
-  public $dbcharset = "utf8";
-  public $captcha = 'recaptcha';
-  public $sitekey = '6LfDkYwbAAAAANLS14699V3YM3JgE5Y4kZUxK3Ym';
-	public $secretkey = '6LfDkYwbAAAAAKoWReBKLlC3VvWVujGx4GlVvJ25';
+	public $dbname = '';
+	public $dbport = "";
+	public $dbcharset = "utf8";
+	public $captcha = 'recaptcha';
+	public $sitekey = '';
+	public $secretkey = '';
 	public $mailer = 'mail';
 	public $mailfrom = 'contact@victorlouvet.fr';
 	public $fromname = 'Contact Victor Louvet';
-  public $forcehttps = false;
+	public $forcehttps = false;
 }
